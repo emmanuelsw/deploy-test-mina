35f611a30405c78764c8c94951fff9f2cfe9450b
EmmanuelSW <edhgt89@hotmail.com>
2018-01-24 19:28:12 -0500

    Deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,18 @@
 require 'mina/git'
 require 'mina/deploy'
 
-set :application_name, 'lock'
+set :application_name, 'secret-code'
 set :user, 'deploy'
 set :domain, '45.55.54.26'
-set :deploy_to, '/var/www/html/lock'
+set :deploy_to, '/var/www/html/secret-code_files'
+set :current_path, '/var/www/html/secret-code'
 set :repository, 'git@github.com:emmanuelsw/deploy-test-mina.git'
 set :branch, 'master'
 
 desc "Deploys the current version to the server."
 task :deploy do
-
   deploy do
     invoke :'git:clone'
     invoke :'deploy:cleanup'
   end
-
 end
